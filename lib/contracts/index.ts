aa8bcd88b3cf28567e3ddf93cff889413462b0e8
Ramiro González Maciel <ramirogm@gmail.com>
2022-05-10 10:03:55 -0300

    Add relationship definitions
    Add relationships defined between contract types in the module,
    or between contract types in the module and its dependencies
    
    Change-type: minor
    Signed-off-by: Ramiro Gonzalez <ramiro.gonzalez@balena.io>

--- lib/contracts/index.ts
+++ lib/contracts/index.ts
@@ -1,8 +1,12 @@
 import type { ContractDefinition } from '@balena/jellyfish-types/build/core';
 import { channelUserFeedback } from './channel-user-feedback';
+import { relationshipPatternIsAttachedToUserFeedback } from './relationship-pattern-is-attached-to-user-feedback';
+import { relationshipUserFeedbackIsOwnedByUser } from './relationship-user-feedback-is-owned-by-user';
 import { userFeedback } from './user-feedback';
 
 export const contracts: ContractDefinition[] = [
 	channelUserFeedback,
+	relationshipPatternIsAttachedToUserFeedback,
+	relationshipUserFeedbackIsOwnedByUser,
 	userFeedback,
 ];
