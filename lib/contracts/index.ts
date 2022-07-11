aece4feda48235cae602f1d3ac3e1230a1f4ace2
Josh Bowling <josh@balena.io>
2022-07-11 11:19:26 +0900

    Add missing message to user-feedback relationship
    Change-type: minor
    Signed-off-by: Josh Bowling <josh@balena.io>

--- lib/contracts/index.ts
+++ lib/contracts/index.ts
@@ -1,11 +1,13 @@
 import type { ContractDefinition } from 'autumndb';
 import { channelUserFeedback } from './channel-user-feedback';
+import { relationshipMessageIsAttachedToUserFeedback } from './relationship-message-is-attached-to-user-feedback';
 import { relationshipPatternIsAttachedToUserFeedback } from './relationship-pattern-is-attached-to-user-feedback';
 import { relationshipUserFeedbackIsOwnedByUser } from './relationship-user-feedback-is-owned-by-user';
 import { userFeedback } from './user-feedback';
 
 export const contracts: ContractDefinition[] = [
 	channelUserFeedback,
+	relationshipMessageIsAttachedToUserFeedback,
 	relationshipPatternIsAttachedToUserFeedback,
 	relationshipUserFeedbackIsOwnedByUser,
 	userFeedback,
