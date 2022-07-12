e75690bc4a3c38732d2513723b44bf416c9b8f57
Josh Bowling <josh@balena.io>
2022-07-12 09:19:42 +0900

    Remove message relationship
    Now using the wildcard message relationship defined in plugin-default
    
    Change-type: major
    Signed-off-by: Josh Bowling <josh@balena.io>

--- lib/contracts/index.ts
+++ lib/contracts/index.ts
@@ -1,13 +1,11 @@
 import type { ContractDefinition } from 'autumndb';
 import { channelUserFeedback } from './channel-user-feedback';
-import { relationshipMessageIsAttachedToUserFeedback } from './relationship-message-is-attached-to-user-feedback';
 import { relationshipPatternIsAttachedToUserFeedback } from './relationship-pattern-is-attached-to-user-feedback';
 import { relationshipUserFeedbackIsOwnedByUser } from './relationship-user-feedback-is-owned-by-user';
 import { userFeedback } from './user-feedback';
 
 export const contracts: ContractDefinition[] = [
 	channelUserFeedback,
-	relationshipMessageIsAttachedToUserFeedback,
 	relationshipPatternIsAttachedToUserFeedback,
 	relationshipUserFeedbackIsOwnedByUser,
 	userFeedback,
